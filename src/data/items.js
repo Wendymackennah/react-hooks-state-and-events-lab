c40b30db6ae1c27c0b4dd596a4f3e354f79a0264
wendymackennah <wendymackennah5@gmail.com>
2024-02-03 17:40:03 +0300

    completed assignment

--- src/data/items.js
+++ src/data/items.js
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
@@ -8,42 +6,4 @@ const items = [
   { id: 5, name: "Cookies", category: "Dessert" },
 ];
 
-function Item({ item, addToCart }) {
-  const [inCart, setInCart] = useState(false);
-
-  const handleButtonClick = () => {
-    setInCart((prevInCart) => !prevInCart);
-    addToCart(item); // Placeholder addToCart function
-  };
-
-  return (
-    <li className={inCart ? "in-cart" : ""}>
-      {item.name} - {item.category}
-      <button onClick={handleButtonClick}>
-        {inCart ? "Remove From Cart" : "Add to Cart"}
-      </button>
-    </li>
-  );
-}
-
-function ShoppingList() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (item) => {
-    // Placeholder addToCart function, you need to implement the actual logic
-    // For example, you can add or remove items from the cart array.
-    setCart((prevCart) =>
-      inCart ? prevCart.filter((i) => i.id !== item.id) : [...prevCart, item]
-    );
-  };
-
-  return (
-    <ul>
-      {items.map((item) => (
-        <Item key={item.id} item={item} addToCart={addToCart} />
-      ))}
-    </ul>
-  );
-}
-
-export default ShoppingList;
+export default items;
